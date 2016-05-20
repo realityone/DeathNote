7d40f322a5071356382a7b1cda2052daf044191e
realityone <realityone@me.com>
2016-05-20 13:53:32 +0800

    add example

---
+++ example/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -ex
+
+echo "Starting Death Note Demo..."
+
+docker-compose -f interlock.yml up -d
+docker-compose -f worker.yml -d
+docker-compose -f demo.yml up -d
+
+echo "Starting Death Note Secceed."
