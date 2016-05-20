408413e11aa8d4347f6d71578a4e230eb8a25b70
realityone <realityone@me.com>
2016-05-20 14:11:24 +0800

    fix start.sh script

--- example/start.sh
+++ example/start.sh
@@ -5,7 +5,7 @@ set -ex
 echo "Starting Death Note Demo..."
 
 docker-compose -f interlock.yml up -d
-docker-compose -f worker.yml -d
+docker-compose -f worker.yml up -d
 docker-compose -f demo.yml up -d
 
 echo "Starting Death Note Secceed."
