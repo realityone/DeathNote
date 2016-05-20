04da7aaac6d512093e90816d665796470eaa7c3d
realityone <realityone@me.com>
2016-05-20 17:33:18 +0800

    fix start_2048.sh script

--- example/2048_test/start_2048.sh
+++ example/2048_test/start_2048.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-for (( i = 0; i < 100; i++ )); do
+set -ex
+
+for (( i = 0; i < 50; i++ )); do
     project="demo_2048_$i"
     cat 2048_base.yml | sed "s/__HOSTNAME__/$project/" | docker-compose -p $project -f - up -d
 done
+
+docker ps --filter=label=interlock.domain=death-note.co -q | xargs docker pause
